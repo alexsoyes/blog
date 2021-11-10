a290b6a0591026f659b39bdb9e563959fc3d6305
Clem34280 <clement.mure@epsi.fr>
2021-11-10 12:29:07 +0100

    Exo 4

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,16 +10,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact/{city}", name="contact")
+     * @Route("/contactez-nous", name="contact")
      */
-    public function contactCity(Request $request, string $city=""): Response
+    public function index(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'name' => 'Alex',
+        ]);
+    }
+
+    /**
+     * @Route("/contacter/{city}", name="contactCity")
+     */
+    public function contactCity(Request $request, string $city): Response
     {
         $name = $request->query->get('name');
 
+        dump($name);
+
         return $this->render('contact/index.html.twig', [
             'name' => $name,
-            'city' => $city,
-            'controller_name' => 'ContactController'
+            'city' => $city
         ]);
     }
 }
